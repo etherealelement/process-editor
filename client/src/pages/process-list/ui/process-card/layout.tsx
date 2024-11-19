7d1c8177e665fcc075934481025d3437d5851036
etherealelement <vitalik.safonov@list.ru>
2024-11-19 06:06:27 +0300

    feat: add api and card

--- client/src/pages/process-list/ui/process-card/layout.tsx
+++ client/src/pages/process-list/ui/process-card/layout.tsx
@@ -1,15 +1,19 @@
 import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
 
 export function Layout({
   onDelete,
   name,
+  id,
 }: {
+  id: string;
   name: string;
   onDelete?: () => void;
 }) {
   return (
     <div className={styles.root}>
       <div className={styles.title}>{name}</div>
+      <Link className={styles.link} to={`/process/${id}`}></Link>
       <button className={styles.deleteBtn} onClick={onDelete}>
         Delete
       </button>
